5735ee120f15cb17617d2d304eef51c24cfae62d
willianbogarin <willianbogarin@hotmail.com>
2023-09-16 11:01:36 +0200

    Initial Commit

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Willian Bogarin Jr on 2023. 09. 16..
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
